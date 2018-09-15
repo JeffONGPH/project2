bc5ec6ddd49e1a176deb623510952e1583c4416c
Joshua Beckford <joshua93beckford@gmail.com>
2018-09-15 13:00:49 -0400

    V 0.0.1

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,38 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/api/examples", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.json(dbExamples);
     });
   });
 
   //  Create a new example
-  app.post("/api/todo", function(req, res) {
-    db.TODO.create(req.body).then(function(dbTODO) {
+  app.post("/api/todo", function (req, res) {
+    db.TODO.create(req.body).then(function (dbTODO) {
       res.json(dbTODO);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/todo/:id", function (req, res) {
+    db.TODO.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
       res.json(dbExample);
     });
+  });
+
+  app.put("/api/todo/:id", function (req, res) {
+    console.log(req.body);
+    db.TODO.update({
+      completed: req.body.completed
+    }, {
+        where: {
+          id: req.params.id
+        }
+      }).then(function (dbPost) {
+        res.json(dbPost);
+        
+      });
   });
 };
